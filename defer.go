9cab4286892c529d81b06a032321fe69da656bbe
Shenali-SJ <shenalijayakody@gmail.com>
2021-07-09 22:36:46 +0530

    complete defer

---
+++ defer.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func greet() {
+	fmt.Println("Welcome")
+}
+
+func printNum() {
+	defer greet() //this is invoked at the end
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println("Printed numbers in order")
+}
+
+func increaseByTen(num int) (sum int) {
+	sum = num + 10
+	fmt.Println("In function ", num)
+	return
+}
+
+func printReverse(word string) {
+	for _, v := range []rune(word) {
+		defer fmt.Printf("%c \n", v)
+	}
+}
+
+func main() {
+	printNum()
+
+	a := 12
+	defer increaseByTen(a)
+	a = 56
+	fmt.Println("Before executing ", a)
+
+	fmt.Println()
+	printReverse("Shenali")
+
+}
